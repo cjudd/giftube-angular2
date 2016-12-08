4b51546f066143d5abbdea228949c0966dd07209
Christopher M. Judd <javajudd@gmail.com>
2016-12-07 19:59:30 -0500

    Generated GifsComponent.

---
+++ src/app/gifs/gifs.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { GifsComponent } from './gifs.component';
+
+describe('GifsComponent', () => {
+  let component: GifsComponent;
+  let fixture: ComponentFixture<GifsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GifsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GifsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/gifs/gifs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs',
+  templateUrl: './gifs.component.html',
+  styleUrls: ['./gifs.component.css']
+})
+export class GifsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
